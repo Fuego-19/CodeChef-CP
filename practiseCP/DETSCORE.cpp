d7ca53354003cc73392e4dab12fa1847e8fe06a8
Mohit <98217406+Fuego-19@users.noreply.github.com>
2022-06-20 23:04:29 +0530

    Add files via upload

---
+++ practiseCP/DETSCORE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t-->0){
+	    int X,N;
+	    cin>>X>>N;
+	    int temp = X/10;
+	    cout<<N*temp<<"\n";
+	   
+	}
+	return 0;
+}
